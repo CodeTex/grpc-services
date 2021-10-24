68b75b73f4a2e4a44df3c6a5a2b1249b5182491b
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-10-24 11:31:08 +0200

    ADD gorest endoint

--- gateway/src/api/endpoints/testing.py
+++ gateway/src/api/endpoints/testing.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import requests
 
 
 router = APIRouter()
@@ -7,3 +8,14 @@ router = APIRouter()
 @router.get("/", include_in_schema=False)
 def index():
     return "You've reached the testing API service"
+
+
+@router.get("/gorest")
+def get_gorest_users():
+    res = requests.get("https://gorest.co.in/public/v1/users")
+    return res.json()
+
+
+@router.get("")
+def a():
+    pass
