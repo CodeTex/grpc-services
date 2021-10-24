e9a2fc0f9ccae918b4f4a13b5437dea815af4f56
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-10-24 11:56:34 +0200

    ADD static folder

--- gateway/src/main.py
+++ gateway/src/main.py
@@ -1,7 +1,8 @@
 from functools import lru_cache
 
 from fastapi import FastAPI
-import uvicorn
+from fastapi.staticfiles import StaticFiles
+from uvicorn import run
 
 from api.api import api_router
 from core.config import Settings
@@ -13,6 +14,7 @@ def get_settings():
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", include_in_schema=False)
@@ -27,7 +29,7 @@ app.include_router(api_router, prefix=get_settings().api_v1_str)
 if __name__ == "__main__":
     settings = get_settings()
     # https://github.com/encode/uvicorn/blob/48edc940522a3d0d7529922a23ac019eeb53f629/uvicorn/config.py#L186
-    uvicorn.run(
+    run(
         app="main:app",
         host=settings.server_host,
         port=settings.server_port,
